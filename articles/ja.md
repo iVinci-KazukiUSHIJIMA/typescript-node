ea7d549add90e574541ae16ea192b2dd5507e0ee
iVinci-KazukiUSHIJIMA <kazuki.ushijima@i-Vinci.co.jp>
2024-02-13 06:32:07 +0000

    doc: 記事追加

--- articles/ja.md
+++ articles/ja.md
@@ -2,10 +2,69 @@
 
 ## 概要
 
-Next.js はリクエストを受け、サーバーサイドレンダリングを行い、レスポンスを受け取ったブラウザはハイドレーションを行い、その後はブラウザ上で再描画が行われる。
+Next.js はリクエストを受け、サーバーサイドレンダリングを行い、レスポンスを受け取ったブラウザはハイドレーションを行い、その後はブラウザ上で再描画が行われる。  
+つまり、ブラウザの JavaScript を Off にして表示した際には下記のようなことが起こるはず。  
+ということを考え、検証してみた  
+[検証で使ったリポジトリ](https://github.com/iVinci-KazukiUSHIJIMA/typescript-node)
+
+## 目次
+
+- [サーバーサイドレンダリング完全に理解したので仮説検証してみた \[Next.js\]](#サーバーサイドレンダリング完全に理解したので仮説検証してみた-nextjs)
+  - [概要](#概要)
+  - [目次](#目次)
+  - [つまりこういうことですよね？検証したい仮説](#つまりこういうことですよね検証したい仮説)
+  - [React では JSX が一切レンダリングされない](#react-では-jsx-が一切レンダリングされない)
+    - [JavaScript ON の状態 (react)](#javascript-on-の状態-react)
+    - [JavaScript OFF の状態 (react)](#javascript-off-の状態-react)
+  - [Next.js ではサーバーサイドレンダリングにより一部描画が行われてレスポンスされる](#nextjs-ではサーバーサイドレンダリングにより一部描画が行われてレスポンスされる)
+    - [JavaScript ON の状態 (next)](#javascript-on-の状態-next)
+    - [JavaScript OFF の状態 (next)](#javascript-off-の状態-next)
+  - [所感](#所感)
 
 ## つまりこういうことですよね？検証したい仮説
 
-- JavaScript を OFF にして開いた場合
-  - React では JSX が一切レンダリングされない。
-  - Next.js ではサーバーサイドレンダリングにより一部描画が行われてレスポンスされる
+JavaScript を OFF にして開いた場合
+
+- React では JSX が一切レンダリングされない。
+- Next.js ではサーバーサイドレンダリングにより一部描画が行われてレスポンスされる
+
+![00-js-off.png](./images/00-js-off.png)
+
+## React では JSX が一切レンダリングされない
+
+これは予想通り。  
+`index.html`に記載の内容のみが表示され、`App.tsx`以下の資材は表示されていない。また、CSS も`main.tsx`から読み込まれているので、スタイルも適用されていない  
+React 資材として記載された部分が全く動いていないという結果。
+
+### JavaScript ON の状態 (react)
+
+![10-react-js-on.png](./images/10-react-js-on.png)
+
+### JavaScript OFF の状態 (react)
+
+![11-react-js-off.png](./images/11-react-js-off.png)
+
+## Next.js ではサーバーサイドレンダリングにより一部描画が行われてレスポンスされる
+
+Next.js においては一部資材がサーバーサイドレンダリングされる  
+下記のことを試してみた
+
+1. レンダリング時刻の表示
+2. 現在時刻を１秒ごとに再描画
+3. URL search parameter に含まれた id によるデータフェッチング
+4. ユーザーがプルダウンメニューで選択した id によるデータフェッチング
+
+これらのうち、`1・3`はサーバーサイドレンダリングにより処理される。つまり、JavaScript OFF でも表示される。  
+`2・4`は client component による処理なので、JavaScript OFF では表示されない。なお、client component の内部であっても、サーバーサイドレンダリングが可能なものはレンダリングされてからレスポンスされる
+
+### JavaScript ON の状態 (next)
+
+![20-next-js-on.png](./images/20-next-js-on.png)
+
+### JavaScript OFF の状態 (next)
+
+![21-next-js-off.png](./images/21-next-js-off.png)
+
+## 所感
+
+理屈を理解しようと務めることも大事。それと同時に、実際に手を動かして仮説を検証したほうが解像度が高まる
--- my-sample-next-app/src/app/img2base64/page.tsx
+++ my-sample-next-app/src/app/img2base64/page.tsx
@@ -4,10 +4,12 @@ import React, { useCallback, useState } from 'react';
 
 export default function Img2Base64() {
   const [imgSrc, setImgSrc] = useState('');
+  const [fileName, setFileName] = useState('');
   const [isImgLoading, setIsImgLoading] = useState(false);
 
   const onSelectFile = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
     if (!e.target.files || e.target.files.length === 0) return;
+    const file = e.target.files[0];
 
     setIsImgLoading(true);
 
@@ -16,7 +18,8 @@ export default function Img2Base64() {
       setImgSrc(reader.result?.toString() ?? '');
       setIsImgLoading(false);
     });
-    reader.readAsDataURL(e.target.files[0]);
+    reader.readAsDataURL(file);
+    setFileName(file.name);
   }, []);
 
   return (
@@ -31,16 +34,24 @@ export default function Img2Base64() {
         accept='image/*'
         onChange={onSelectFile}
       />
-      <Result base64Str={imgSrc} isImgLoading={isImgLoading} />
+      <div className='mt-2'>
+        <Result
+          base64Str={imgSrc}
+          fileName={fileName}
+          isImgLoading={isImgLoading}
+        />
+      </div>
     </>
   );
 }
 
 function Result({
   base64Str,
+  fileName,
   isImgLoading,
 }: {
   base64Str: string;
+  fileName: string;
   isImgLoading: boolean;
 }) {
   if (isImgLoading) return <p>loading...</p>;
@@ -48,13 +59,23 @@ function Result({
 
   return (
     <>
-      <h2>preview</h2>
-      {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img src={base64Str} alt='previewForBase64EncodedImage' />
-      <h2>base64 encoded string</h2>
-      <textarea name='base64-encoded-string' className='w-full' rows={10}>
-        {base64Str}
-      </textarea>
+      <div>
+        <h2>preview</h2>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img src={base64Str} alt='previewForBase64EncodedImage' />
+      </div>
+      <div className='mt-2'>
+        <h2>base64 encoded string</h2>
+        <textarea name='base64-encoded-string' className='w-full' rows={10}>
+          {base64Str}
+        </textarea>
+      </div>
+      <div className='mt-2'>
+        <h2>with markdown</h2>
+        <textarea name='base64-encoded-string' className='w-full' rows={10}>
+          {`![${fileName}](${base64Str} "${fileName}")`}
+        </textarea>
+      </div>
     </>
   );
 }
